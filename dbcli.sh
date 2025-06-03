da89c32681acac30425cd8429da161ac99a51b04
NothingButSeaweedSoup <981885195@qq.com>
2025-06-03 15:19:45 +0800

    修改Shell

---
+++ dbcli.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+JAVA_CLASSPATH="target/classes"
+JAVA_MAIN_CLASS="cli.CommandLineInterface"
+DATA_PATH="data"
+
+java -cp $JAVA_CLASSPATH $JAVA_MAIN_CLASS $DATA_PATH
+
+exit_status=$?
+
+if [ $exit_status -ne 0 ]; then
+    echo "Java程序启动失败，退出状态码: $exit_status"
+    exit $exit_status
+fi
+
+exit 0
