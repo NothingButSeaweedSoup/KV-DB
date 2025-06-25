ab657f8eba4458a30de991094e45a05b94bd6257
NothingButSeaweedSoup <981885195@qq.com>
2025-06-25 08:30:02 +0800

    修改集群模式：基于Socket定时任务完成批量同步

--- src/main/java/cluster/ClusterManager.java
+++ src/main/java/cluster/ClusterManager.java
@@ -11,17 +11,35 @@ public class ClusterManager {
         this.nodes = nodes;
     }
 
-    public void syncData(byte[] data) {
+    // 修改syncData方法以接受List<byte[]>类型的参数
+    public void syncData(List<byte[]> dataList) {
         for (ClusterNode node : nodes) {
             if (!node.isMaster()) {
                 try (Socket socket = new Socket(node.getHost(), node.getPort())) {
                     try (OutputStream out = socket.getOutputStream()) {
-                        out.write(data);
+                        // 写入数据列表的大小
+                        out.write(intToBytes(dataList.size()));
+                        for (byte[] data : dataList) {
+                            // 写入每个数据的长度
+                            out.write(intToBytes(data.length));
+                            // 写入数据本身
+                            out.write(data);
+                        }
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
+
+    // 辅助方法：将int转换为4字节的byte数组
+    private byte[] intToBytes(int value) {
+        return new byte[]{
+                (byte) (value >> 24),
+                (byte) (value >> 16),
+                (byte) (value >> 8),
+                (byte) value
+        };
     }
 }
--- src/main/java/cluster/SlaveNode.java
+++ src/main/java/cluster/SlaveNode.java
@@ -35,20 +35,21 @@ public class SlaveNode extends LSMStorageEngine {
         @Override
         public void run() {
             try (InputStream in = clientSocket.getInputStream()) {
-                byte[] data = new byte[1024];
-                int bytesRead;
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                while ((bytesRead = in.read(data)) != -1) {
-                    baos.write(data, 0, bytesRead);
-                }
-                byte[] receivedData = baos.toByteArray();
-                // 处理接收到的数据
-                try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(receivedData))) {
-                    Object value = ois.readObject();
-                    // 更新本地存储
-                    put("received_key".getBytes(), value);
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
+                // 读取数据列表的大小
+                int dataSize = bytesToInt(readBytes(in, 4));
+                for (int i = 0; i < dataSize; i++) {
+                    // 读取每个数据的长度
+                    int dataLength = bytesToInt(readBytes(in, 4));
+                    // 读取数据本身
+                    byte[] data = readBytes(in, dataLength);
+                    // 处理接收到的数据
+                    try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) {
+                        Object value = ois.readObject();
+                        // 更新本地存储
+                        put("received_key".getBytes(), value);
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -59,6 +60,28 @@ public class SlaveNode extends LSMStorageEngine {
                     e.printStackTrace();
                 }
             }
+        }
+
+        // 辅助方法：从InputStream中读取指定长度的字节
+        private byte[] readBytes(InputStream in, int length) throws IOException {
+            byte[] buffer = new byte[length];
+            int bytesRead = 0;
+            while (bytesRead < length) {
+                int result = in.read(buffer, bytesRead, length - bytesRead);
+                if (result == -1) {
+                    throw new IOException("Unexpected end of stream");
+                }
+                bytesRead += result;
+            }
+            return buffer;
+        }
+
+        // 辅助方法：将4字节的byte数组转换为int
+        private int bytesToInt(byte[] bytes) {
+            return (bytes[0] << 24) |
+                    (bytes[1] << 16) |
+                    (bytes[2] << 8) |
+                    (bytes[3] & 0xFF);
         }
     }
 }
